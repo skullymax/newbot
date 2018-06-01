c68b95d766d4e6849bae565f290f6ccde189b8d5
skullymax <38359146+skullymax@users.noreply.github.com>
2018-06-01 21:41:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,17 +7,17 @@ const fs = require("fs");
 
 client.on("ready", () => {
     console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    client.user.setActivity(`/help | New forum!`);
+    client.user.setActivity(`/help | www.eyzalts.us`);
 });
 
 client.on("guildCreate", guild => {
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-    client.user.setActivity(`/help | New forum!`);
+    client.user.setActivity(`/help | www.eyzalts.us`);
 });
 
 client.on("guildDelete", guild => {
     console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    client.user.setActivity(`/help | New forum!`);
+    client.user.setActivity(`/help | www.eyzalts.us`);
 });
 
 function altmc() {
